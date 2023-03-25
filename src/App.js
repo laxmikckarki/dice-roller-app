4253ec56e1a398d7728bfd30c76a6966eb9a943f
laxmikckarki <108900563+laxmikckarki@users.noreply.github.com>
2023-03-25 14:05:47 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
-const App =() => {
-const[diceNumber, setDiceNumber] = useState(1)
-const[diceNumber2, setDiceNumber2] = useState(2)
+const App = () => {
+  const [diceNumber, setDiceNumber] = useState(1);
+  const [diceNumber2, setDiceNumber2] = useState(2);
 
-
-const refreshDice = () => {
-  const ranno = Math.floor(Math.random() * 6)+1
-  setDiceNumber(ranno)
-  const ranno2 = Math.floor(Math.random() * 6)+1
-  setDiceNumber2(ranno2)
-}
+  const refreshDice = () => {
+    const ranno = Math.floor(Math.random() * 6) + 1;
+    setDiceNumber(ranno);
+    const ranno2 = Math.floor(Math.random() * 6) + 1;
+    setDiceNumber2(ranno2);
+  };
   return (
     <div>
-<img width={300} height={300} src={require(`./assets/${diceNumber}.png`)}></img>
-<img width={300} height={300} src={require(`./assets/${diceNumber2}.png`)}></img>
-<br/>
-<button onClick ={() => refreshDice()} className='button'>Roll</button>
-
+      <img
+        alt="dice"
+        width={300}
+        height={300}
+        src={require(`./assets/${diceNumber}.png`)}
+      ></img>
+      <img
+        alt="dice"
+        width={300}
+        height={300}
+        src={require(`./assets/${diceNumber2}.png`)}
+      ></img>
+      <br />
+      <button onClick={() => refreshDice()} className="button">
+        Roll
+      </button>
     </div>
   );
-}
+};
 
 export default App;
